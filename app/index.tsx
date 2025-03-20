1eaf05b5b13187566eea11f9d0bdf449a58d0e23
edenWerke <edenw6292@gmail.com>
2025-03-20 15:12:14 +0300

    not found page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function Index() {
   return (
     <View style={styles.container}>
+      
       <Text style={styles.text}>Home screen</Text>
+    <Link style={styles.link} href='/about'> Go To About Page</Link>
+    {/* <Link style={styles.link} href='/+not-found'> Go To About Page</Link> */}
     </View>
   );
 }
@@ -11,11 +14,16 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     color: '#fff',
   },
+  link:{
+    fontSize:20,
+    textDecorationLine:'none',
+    color:'pink'
+  }
 });

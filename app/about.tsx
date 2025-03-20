4d1d3f3dbfa4731af589005ba99e106f89776863
edenWerke <edenw6292@gmail.com>
2025-03-20 14:39:05 +0300

    about page

---
+++ app/about.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AboutScreen(){
+    return(
+        <View style={styles.container}>
+
+
+<Text style={styles.text}>About screen</Text>
+
+
+        </View>
+    )
+}
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'dodgerblue',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    text:{
+        color:'orangered'
+    }
+})

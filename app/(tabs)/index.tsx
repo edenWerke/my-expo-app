516ff78e66665ea231cd15a389f73f96782bf49d
edenWerke <edenw6292@gmail.com>
2025-03-22 22:17:14 +0300

    Emoji List

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,81 +1,22 @@
-// import * as ImagePicker from 'expo-image-picker';
-// import { useState } from 'react';
-// import { StyleSheet, View } from 'react-native';
 
-// import Button from '@/components/Button';
-// import ImageViewer from '@/components/ImageViewer';
-
-// const PlaceHolderImage = require("../../assets/images/background.jpg");
-
-// export default function Index() {
-//   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-//   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
-
-//   const pickImageAsync = async () => {
-//     let result = await ImagePicker.launchImageLibraryAsync({
-//       mediaTypes: ['images'],
-//       allowsEditing: true,
-//       quality: 1,
-//     });
-//     const onReset=()=>{
-//         setShowAppOptions(false);
-//     }
-
-//     if (!result.canceled) {
-//       setSelectedImage(result.assets[0].uri);
-//       setShowAppOptions(true);
-//     } else {
-//       alert('You did not select any image.');
-//     }
-//   };
-
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.imageContainer}>
-//         <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage} />
-//       </View>
-//       {showAppOptions ? (
-//         <View />
-//       ) : (
-//         <View style={styles.footerContainer}>
-//           <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-//           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
-//         </View>
-//       )}
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#25292e',
-//     alignItems: 'center',
-//   },
-//   imageContainer: {
-//     flex: 1,
-//   },
-//   footerContainer: {
-//     flex: 1 / 3,
-//     alignItems: 'center',
-//   },
-// });
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer';
-
 import CircleButton from '@/components/CircleButton';
 import IconButton from '@/components/IconButton';
+import ImageViewer from '@/components/ImageViewer';
+
+import EmojiPicker from '@/components/EmojiPicker';
 
 
-const PlaceHolderImage = require("../../assets/images/background.jpg");
+const PlaceholderImage = require("../../assets/images/background.jpg");
 
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -97,7 +38,11 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -107,7 +52,7 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -123,6 +68,9 @@ export default function Index() {
           <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* A list of emoji component will go here */}
+      </EmojiPicker>
     </View>
   );
 }
